4f40e1a55f07b9e0d8a67d506e212e5e5a983282
Aymane <aymane@cashflowpositifi.fr>
2024-09-17 13:23:50 +0200

    Setup mongoDB

---
+++ apps/api/src/env.ts
@@ -0,0 +1,20 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+const STRING = z.string().min(1);
+const NUMBER = z.coerce.number();
+const BOOLEAN = z.enum(["true", "false"]).transform((s) => s === "true");
+
+export const env = createEnv({
+    server: {
+        PORT: NUMBER.int().default(3000),
+        MONGODB_URI: STRING.url(),
+        ENABLE_SWAGGER: BOOLEAN.default("false"),
+        ACCESS_TOKEN_SECRET: STRING,
+        REFRESH_TOKEN_SECRET: STRING,
+        REFRESH_TOKEN_EXPIRATION_IN_MS: NUMBER.int().default(1000 * 60 * 60 * 24 * 7), // 7 days
+    },
+    runtimeEnv: process.env,
+    emptyStringAsUndefined: true,
+    skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+});
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,15 @@
-import { createExpressApp } from "@/application/app.js";
+import {createExpressApp} from "@/application/app.js";
+import {connectToMongoDb} from "@/infrastructure/database/database.js";
 
 async function main() {
-  const app = createExpressApp();
+    const {env} = await import("./env.js");
+    const app = createExpressApp();
 
-  app.listen()
+    await connectToMongoDb(env.MONGODB_URI, {});
+
+    app.listen(env.PORT, () => {
+        console.log(`Example app listening on port ${env.PORT}`)
+    })
 }
+
+await main()
